9f285082fd24890e9781a4a3b69a887ffa2240b2
boufni95 <boufni95@gmail.com>
2025-08-01 15:50:25 +0000

    noffer receipt

--- src/noffer.ts
+++ src/noffer.ts
@@ -1,40 +1,19 @@
 import { nip44, getPublicKey, finalizeEvent } from "nostr-tools"
 import { AbstractSimplePool, SubCloser } from "nostr-tools/lib/types/pool"
 import { sendRequest } from "./sender.js"
-const { getConversationKey, decrypt, encrypt } = nip44
+const { getConversationKey, encrypt } = nip44
 
 export type NofferData = { offer: string, amount?: number, zap?: string, payer_data?: any }
 export type NofferSuccess = { bolt11: string }
 export type NofferError = { code: number, error: string, range: { min: number, max: number } }
 export type NofferResponse = NofferSuccess | NofferError
+export type NofferReceipt = { preimage?: string }
 
-/* export const SendNofferRequest = async (pool: AbstractSimplePool, privateKey: Uint8Array, relays: string[], toPubKey: string, data: NofferData, timeoutSeconds = 30): Promise<NofferResponse> => {
+export const SendNofferRequest = async (pool: AbstractSimplePool, privateKey: Uint8Array, relays: string[], toPubKey: string, data: NofferData, timeoutSeconds = 30, onReceipt?: (receipt: NofferReceipt) => void): Promise<NofferResponse> => {
     const publicKey = getPublicKey(privateKey)
     const content = encrypt(JSON.stringify(data), getConversationKey(privateKey, toPubKey))
     const event = newNofferEvent(content, publicKey, toPubKey)
-    const signed = finalizeEvent(event, privateKey)
-    await Promise.all(pool.publish(relays, signed))
-    return new Promise<NofferResponse>((res, rej) => {
-        let closer: SubCloser = { close: () => { } }
-        const timeout = setTimeout(() => {
-            closer.close(); rej("failed to get noffer response in time")
-        }, timeoutSeconds * 1000)
-
-        closer = pool.subscribeMany(relays, [newNofferFilter(publicKey, signed.id)], {
-            onevent: async (e) => {
-                clearTimeout(timeout)
-                const content = decrypt(e.content, getConversationKey(privateKey, toPubKey))
-                res(JSON.parse(content))
-            }
-        })
-    })
-} */
-
-export const SendNofferRequest = async (pool: AbstractSimplePool, privateKey: Uint8Array, relays: string[], toPubKey: string, data: NofferData, timeoutSeconds = 30): Promise<NofferResponse> => {
-    const publicKey = getPublicKey(privateKey)
-    const content = encrypt(JSON.stringify(data), getConversationKey(privateKey, toPubKey))
-    const event = newNofferEvent(content, publicKey, toPubKey)
-    return sendRequest(pool, { privateKey, publicKey }, relays, toPubKey, event, 21001, timeoutSeconds)
+    return sendRequest(pool, { privateKey, publicKey }, relays, toPubKey, event, 21001, timeoutSeconds, onReceipt)
 }
 
 export const newNofferEvent = (content: string, fromPub: string, toPub: string) => ({
